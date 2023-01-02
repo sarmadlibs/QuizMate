d0cfb4f506c7105a77b79997237a572c8c09fe16
Sarmad <afzalsarmad77@gmail.com>
2023-01-02 12:23:27 -0500

    added flipping functionality to cards, also allowed cards to be added on top of each other instead of replacing

--- src/StudyCards.js
+++ src/StudyCards.js
@@ -1,25 +1,131 @@
+// import React, { useState } from "react";
+// import "./StudyCards.css";
+
+// function StudyCards() {
+//   // Declare state variables "input", "questionInput", and "answerInput"
+//   const [input, setInput] = useState("");
+//   const [questionInput, setQuestionInput] = useState("");
+//   const [answerInput, setAnswerInput] = useState("");
+
+//   // Declare the "cards" state variable to hold the array of study cards
+//   const [cards, setCards] = useState([]);
+
+//   // Event handler for the textarea element
+//   const handleInputChange = (event) => {
+//     setInput(event.target.value);
+//   };
+
+//   // Event handlers for the question and answer input fields
+//   const handleQuestionChange = (event) => {
+//     setQuestionInput(event.target.value);
+//   };
+
+//   const handleAnswerChange = (event) => {
+//     setAnswerInput(event.target.value);
+//   };
+
+//   // Event handler for the form submission
+//   const handleInputSubmit = (event) => {
+//     event.preventDefault();
+
+//     /**
+//      *! Split the input text into an array of lines,
+//      *! Filter out empty lines from the input lines,
+//      *! Create array of study cards from the input lines
+//      **/
+//     const inputLines = input.split("\n");
+//     const filteredInputLines = inputLines.filter((line) => line !== "");
+//     const newCards = filteredInputLines.reduce((acc, line, index) => {
+//       // If the current index is even, add a new study card to the array
+//       if (index % 2 === 0) {
+//         acc.push({
+//           question: line,
+//           answer: filteredInputLines[index + 1],
+//         });
+//       }
+//       return acc;
+//     }, []);
+
+//     //! Update the "cards" state with the new array of study cards
+//     setCards([...cards, ...newCards]);
+//   };
+
+//   // Event handler for the "Add card" button
+//   const handleAddCard = (event) => {
+//     event.preventDefault();
+
+//     // Create a new study card object with the question and answer input values
+//     const newCard = {
+//       question: questionInput,
+//       answer: answerInput,
+//     };
+
+//     // Add the new study card to the "cards" state array
+//     setCards([...cards, newCard]);
+
+//     // Clear the question and answer input fields
+//     setQuestionInput("");
+//     setAnswerInput("");
+//   };
+
+//   return (
+//     <div className="App">
+//       <form onSubmit={handleInputSubmit}>
+//         <textarea value={input} onChange={handleInputChange} />
+//         <button type="submit">Create cards</button>
+//       </form>
+//       <form onSubmit={handleAddCard}>
+//         <label>
+//           Question:
+//           <input
+//             type="text"
+//             value={questionInput}
+//             onChange={handleQuestionChange}
+//           />
+//         </label>
+//         <label>
+//           Answer:
+//           <input
+//             type="text"
+//             value={answerInput}
+//             onChange={handleAnswerChange}
+//           />
+//         </label>
+//         <button type="submit">Add card</button>
+//       </form>
+//       {cards.map((card, index) => (
+//         <div key={index} className="card">
+//           <div className="card-front">{card.question}</div>
+//           <div className="card-back">{card.answer}</div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default StudyCards;
+
 import React, { useState } from "react";
 import "./StudyCards.css";
 
 function StudyCards() {
-  // Declare state variables "input", "questionInput", and "answerInput"
+  // Declare state variables "input" and "cards"
   const [input, setInput] = useState("");
+  const [cards, setCards] = useState([]);
   const [questionInput, setQuestionInput] = useState("");
   const [answerInput, setAnswerInput] = useState("");
-
-  // Declare the "cards" state variable to hold the array of study cards
-  const [cards, setCards] = useState([]);
 
   // Event handler for the textarea element
   const handleInputChange = (event) => {
     setInput(event.target.value);
   };
 
-  // Event handlers for the question and answer input fields
+  // Event handler for the question input field
   const handleQuestionChange = (event) => {
     setQuestionInput(event.target.value);
   };
 
+  // Event handler for the answer input field
   const handleAnswerChange = (event) => {
     setAnswerInput(event.target.value);
   };
@@ -52,20 +158,34 @@ function StudyCards() {
 
   // Event handler for the "Add card" button
   const handleAddCard = (event) => {
+    // Prevent the default form submission behavior
     event.preventDefault();
 
-    // Create a new study card object with the question and answer input values
-    const newCard = {
-      question: questionInput,
-      answer: answerInput,
-    };
+    // Add a new study card to the state
+    setCards([
+      ...cards,
+      {
+        question: questionInput,
+        answer: answerInput,
+      },
+    ]);
 
-    // Add the new study card to the "cards" state array
-    setCards([...cards, newCard]);
-
-    // Clear the question and answer input fields
+    // Clear the input fields
     setQuestionInput("");
     setAnswerInput("");
+  };
+
+  // Event handler for flipping a card
+  const handleFlipCard = (card) => {
+    // Find the index of the card in the "cards" array
+    const cardIndex = cards.indexOf(card);
+
+    // Create a new array of cards with the "flipped" class added to the card
+    const newCards = [...cards];
+    newCards[cardIndex] = { ...card, flipped: !card.flipped };
+
+    // Update the "cards" state with the new array of cards
+    setCards(newCards);
   };
 
   return (
@@ -75,26 +195,26 @@ function StudyCards() {
         <button type="submit">Create cards</button>
       </form>
       <form onSubmit={handleAddCard}>
-        <label>
-          Question:
-          <input
-            type="text"
-            value={questionInput}
-            onChange={handleQuestionChange}
-          />
-        </label>
-        <label>
-          Answer:
-          <input
-            type="text"
-            value={answerInput}
-            onChange={handleAnswerChange}
-          />
-        </label>
+        <input
+          type="text"
+          value={questionInput}
+          onChange={handleQuestionChange}
+          placeholder="Question"
+        />
+        <input
+          type="text"
+          value={answerInput}
+          onChange={handleAnswerChange}
+          placeholder="Answer"
+        />
         <button type="submit">Add card</button>
       </form>
       {cards.map((card, index) => (
-        <div key={index} className="card">
+        <div
+          key={index}
+          className={`card ${card.flipped ? "flipped" : ""}`}
+          onClick={() => handleFlipCard(card)}
+        >
           <div className="card-front">{card.question}</div>
           <div className="card-back">{card.answer}</div>
         </div>
