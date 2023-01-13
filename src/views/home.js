9990dcf49d68dd1a312713ecf14041b5ca9a3863
Sarmad <afzalsarmad77@gmail.com>
2023-01-13 12:01:38 -0500

    added welcome message

--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,21 @@
-// import { Card } from "../components/Card";
 import homeCss from "../styles/home.module.css";
+// import { motion } from "framer-motion";
 
 export function Homepage(props) {
   return (
     <div>
+      <div className={homeCss.welcomeContainer}>
+        <div className={homeCss.title}>
+          <div>Welcome to Prep Pal!</div>
+        </div>
+        <div className={homeCss.welcomeMessage}>
+          Use our study card generator to easily create personalized flashcards
+          and achieve academic success. To get started, input your desired text
+          and let our system do the rest. Join the thousands of students who
+          have transformed their study habits and reached their goals with Prep
+          Pal. Let's begin your journey to success today!
+        </div>
+      </div>
       <div className={homeCss.ellipseContainer}>
         <div className={homeCss.svgEllipse}></div>
       </div>
