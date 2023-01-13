1bc89e34d612047e6ca39e8d4d7b3869d6af34d1
Sarmad <afzalsarmad77@gmail.com>
2023-01-12 22:00:53 -0500

    implemented background gradient + moon

--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,15 @@
-import { Card } from "../components/Card";
+// import { Card } from "../components/Card";
+import homeCss from "../styles/home.module.css";
 
-
-export function Homepage (props){
-    return(
-        <Card></Card>
-    )
-
+export function Homepage(props) {
+  return (
+    <div>
+      <div className={homeCss.ellipseContainer}>
+        <div className={homeCss.svgEllipse}></div>
+      </div>
+      <div className={homeCss.constellationContainer}>
+        <div className={homeCss.svgConstellation}></div>
+      </div>
+    </div>
+  );
 }
