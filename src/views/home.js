e45d8c8cd5fa1a6e3c90cb6905731ce31371c026
Sarmad <afzalsarmad77@gmail.com>
2023-01-15 00:31:08 -0500

    added star animation + refined positioning for responsiveness

--- src/views/home.js
+++ src/views/home.js
@@ -1,12 +1,22 @@
+import React from "react";
+import Lottie from "react-lottie";
 import homeCss from "../styles/home.module.css";
+
 // import { motion } from "framer-motion";
+import shootingStar from "../assets/img/shooting-stars.json";
+import starFormation from "../assets/img/star-formation.json";
 
 export function Homepage(props) {
   return (
     <div>
       <div className={homeCss.welcomeContainer}>
-        <div className={homeCss.title}>
-          <div>Welcome to Prep Pal</div>
+        <div className={homeCss.titleContainer}>
+          <div className={homeCss.title}>
+            <div>Welcome to Prep Pal</div>
+          </div>
+          <div className={homeCss.starFormation}>
+            <Lottie options={{ animationData: starFormation }} />
+          </div>
         </div>
         <div className={homeCss.welcomeMessage}>
           Use our study card generator to easily create personalized flashcards
@@ -18,7 +28,12 @@ export function Homepage(props) {
         </div>
       </div>
       <div className={homeCss.ellipseContainer}>
-        <div className={homeCss.svgEllipse}></div>
+        <svg
+          className={homeCss.svgEllipse}
+          width="100%"
+          height="100%"
+          src="../assets/img/Ellipse.svg"
+        ></svg>
       </div>
       <div className={homeCss.constellationContainer}>
         <div className={homeCss.svgConstellation}></div>
