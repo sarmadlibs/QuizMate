f95e2602f9d5ed9afddf0f761391267c8dd098f4
Sarmad <afzalsarmad77@gmail.com>
2023-04-26 23:24:27 +0300

    added quotes and routing to studycard page

--- src/views/landing.js
+++ src/views/landing.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 import Lottie from "react-lottie";
-import anime from "animejs/lib/anime.es.js";
-import homeCss from "../styles/home.module.css";
+import Letterize from "letterizejs";
+import Typewriter from "typewriter-effect/dist/core";
 
-// import { motion } from "framer-motion";
+import anime from "animejs/lib/anime.es.js";
+import landingCss from "../styles/landing.module.css";
+
 import shootingStar from "../assets/img/shooting-stars.json";
 import starFormation from "../assets/img/star-formation.json";
 import astro from "../assets/img/astro.json";
 
-export function Homepage(props) {
+export function LandingPage(props) {
   const titleRef = useRef(null);
   const messageRef = useRef(null);
+  const animateMeRef = useRef(null);
+  const typewriterRef = useRef(null);
+
+  const quotes = [
+    "Believe you can and you're halfway there.",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "The secret of getting ahead is getting started.",
+    "The only way to do great work is to love what you do.",
+    "Don't let yesterday take up too much of today.",
+    "The harder you work for something, the greater you'll feel when you achieve it.",
+    "The way to get started is to quit talking and begin doing.",
+    "The best way to predict the future is to create it.",
+    "You learn more from failure than from success. Don't let it stop you. Failure builds character.",
+  ];
 
   useEffect(() => {
     // First animation on titleRef
@@ -63,7 +80,35 @@ export function Homepage(props) {
         });
       },
     });
+    const test = new Letterize({
+      targets: animateMeRef.current,
+    });
+    console.log(animateMeRef.current);
+
+    var animation = anime.timeline({
+      targets: test.listAll,
+      delay: anime.stagger(50),
+      loop: true,
+    });
+
+    animation
+      .add({
+        translateY: -40,
+      })
+      .add({
+        translateY: 0,
+      });
+
+    startTypewriter();
   }, []);
+
+  function startTypewriter() {
+    new Typewriter(typewriterRef.current, {
+      strings: quotes,
+      autoStart: true,
+      loop: true,
+    });
+  }
 
   useEffect(() => {
     // Get the text and split it into individual characters
@@ -104,15 +149,17 @@ export function Homepage(props) {
 
   return (
     <div>
-      <div className={homeCss.welcomeContainer}>
-        <div className={homeCss.titleContainer}>
-          <div className={homeCss.title} ref={titleRef}>
-            <div>Welcome to quizMAte</div>
+      <div className={landingCss.welcomeContainer}>
+        <div className={landingCss.titleContainer}>
+          <div className={landingCss.title} ref={titleRef}>
+            <div ref={animateMeRef} id="animateMe">
+              Welcome to quizMAte
+            </div>
           </div>
-          <div className={homeCss.starFormation}>
+          <div className={landingCss.starFormation}>
             <Lottie options={{ animationData: starFormation }} />
           </div>
-          <div className={homeCss.astro}>
+          <div className={landingCss.astro}>
             <Lottie
               options={{
                 animationData: astro,
@@ -122,30 +169,38 @@ export function Homepage(props) {
             />
           </div>
         </div>
-        <div className={homeCss.welcomeMessage}>
+        <div className={landingCss.welcomeMessage}>
           Use our study card generator to easily create personalized flashcards
           and achieve academic success. Join the thousands of students who have
           transformed their study habits and reached their goals with Quizmate!
         </div>
-        <div className={homeCss.beginJourney} ref={messageRef}>
-          Let's begin your journey to success today!
+        <div className={landingCss.beginJourney}>
+          <Link
+            className={landingCss.beginJourneyLink}
+            to="/studycards"
+            ref={messageRef}
+          >
+            Let's begin your journey to success today!
+          </Link>
         </div>
+
+        <div className={landingCss.typewriter} ref={typewriterRef}></div>
       </div>
 
-      <div className={homeCss.ellipseContainer}>
+      <div className={landingCss.ellipseContainer}>
         <svg
-          className={homeCss.svgEllipse}
+          className={landingCss.svgEllipse}
           width="100%"
           height="100%"
           src="../assets/img/Ellipse.svg"
         ></svg>
       </div>
-      <div className={homeCss.constellationContainer}>
-        <div className={homeCss.svgConstellation}></div>
+      <div className={landingCss.constellationContainer}>
+        <div className={landingCss.svgConstellation}></div>
       </div>
-      <div className={homeCss.entireConstellationContainer}>
-        <div className={homeCss.svgEntireConstellation}></div>
-        <div className={homeCss.svgEntireConstellation2}></div>
+      <div className={landingCss.entireConstellationContainer}>
+        <div className={landingCss.svgEntireConstellation}></div>
+        <div className={landingCss.svgEntireConstellation2}></div>
       </div>
     </div>
   );
