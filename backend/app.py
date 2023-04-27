2cd739d0a6b223e0827fcbb326f4e7be34526777
Sarmad <afzalsarmad77@gmail.com>
2023-04-27 09:42:57 +0300

    added temperature parameter to control the randomness during sampling + filtered results by top 5 relevance

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,4 @@
 
-
 from transformers import T5ForConditionalGeneration, T5Tokenizer, pipeline
 from flask import Flask, request, jsonify
 
@@ -16,20 +15,23 @@ def generate_qa_pairs():
 
     prompt = f"generate questions for the following text: {text}"
 
-
     inputs = tokenizer.encode(prompt, return_tensors="pt", max_length=512, truncation=True)
-    outputs = model.generate(inputs, max_length=200, num_return_sequences=5, do_sample=True, top_k=100, top_p=0.9)
+    outputs = model.generate(inputs, max_length=200, num_return_sequences=10, do_sample=True, top_k=100, top_p=0.9, temperature=0.8)
 
-    decoded_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    decoded_output = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs]
 
-    questions = decoded_output.strip().split("\n")
+    questions = [question.strip() for output in decoded_output for question in output.split("\n")]
 
-    result = []
+    # Filter out questions with low scores
+    qa_results = []
     for question in questions:
         answer = qa_pipeline(question=question, context=text)
-        result.append({"question": question, "answer": answer["answer"]})
+        qa_results.append({"question": question, "answer": answer["answer"], "score": answer["score"]})
 
-    return jsonify(result)
+    # Sort by score and retain top 5 questions
+    sorted_qa_results = sorted(qa_results, key=lambda x: x["score"], reverse=True)[:5]
+
+    return jsonify(sorted_qa_results)
 
 if __name__ == '__main__':
     app.run(debug=True)
