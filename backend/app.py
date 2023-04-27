1a6130088125754e9fa2bc0bbfbc3761065a9234
Sarmad <afzalsarmad77@gmail.com>
2023-04-27 09:28:50 +0300

    trying to utilize 'valhalla/t5-base-qg-hl' instead of 'valhalla/t5-small-qg-hl'. removed unnecessary </s> and <pad> tokens

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 tokenizer = T5Tokenizer.from_pretrained("t5-base")
-model = T5ForConditionalGeneration.from_pretrained("valhalla/t5-small-qg-hl")
+model = T5ForConditionalGeneration.from_pretrained("valhalla/t5-base-qg-hl")
 qa_pipeline = pipeline("question-answering")
 
 @app.route('/generate_qa_pairs', methods=['POST'])
@@ -15,14 +15,13 @@ def generate_qa_pairs():
 
     prompt = f"generate questions for the following text: {text}"
 
-    # Encode the prompt and generate a response
     inputs = tokenizer.encode(prompt, return_tensors="pt", max_length=512, truncation=True)
     outputs = model.generate(inputs, max_length=200, num_return_sequences=5, do_sample=True, top_k=50)
 
-    decoded_output = tokenizer.decode(outputs[0])
+    decoded_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    # Remove the unnecessary text and split into lines
-    questions = decoded_output.replace("<pad> ", "").strip().split("\n")
+
+    questions = decoded_output.strip().split("\n")
 
 
     result = []
